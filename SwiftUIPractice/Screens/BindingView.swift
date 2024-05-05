eebc3e8297323911b5d793fd307e6a1d2de16b1a
Robert Gravina <robert.gravina@gmail.com>
2024-05-05 22:36:57 +1000

    Remove scrollview from binding view example

--- SwiftUIPractice/Screens/BindingView.swift
+++ SwiftUIPractice/Screens/BindingView.swift
@@ -16,6 +16,7 @@ struct BindingView: View {
             VStack {
                 Text("This view contains a grid of views that have a @Binding to @State on the parent view. This is why we can print the list of colors below. Tap any icon to chose a random pastel color.")
                     .padding()
+                Spacer()
                 LazyVGrid(columns: [
                     GridItem(.flexible()),
                     GridItem(.flexible()),
@@ -25,13 +26,13 @@ struct BindingView: View {
                 ], spacing: 10) {
                     ForEach(colors, id: \.cgColor) { color in
                         Text("\(color)")
-                            .font(.caption)
+                            .font(.system(size: 11, weight: .regular, design: .monospaced))
                             .foregroundColor(color)
                             .onReceive(inspection.notice) { self.inspection.visit(self, $0) }
                     }
                 }
             }
-            ScrollView {
+            VStack {
                 LazyVGrid(columns: [
                     GridItem(.flexible()),
                     GridItem(.flexible()),
