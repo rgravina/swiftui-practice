bf1c204b2c8bcf52722afcfc42a8b0212a0fbb6c
Robert Gravina <robert.gravina@gmail.com>
2024-05-06 08:55:05 +1000

    Use adaptive grid rows

--- SwiftUIPractice/Screens/BindingView.swift
+++ SwiftUIPractice/Screens/BindingView.swift
@@ -18,21 +18,17 @@ struct BindingView: View {
     var body: some View {
         VStack {
             VStack {
-                Text("This view contains a grid of views that have a @Binding to @State on the parent view. The colors can be changed in the parent via the \"Regenerate Colors\" button or in the subviews via tapping on any runner icon.")
+                Text("Subviews use @Binding to a @State array on the parent view. The colors can be changed in the parent via the \"Regenerate Colors\" button or via tapping on any runner icon.")
                 Spacer()
                 Button("Rengenerate Colors") {
                     regenerateColors()
                 }
                 LazyVGrid(columns: [
-                    GridItem(.flexible()),
-                    GridItem(.flexible()),
-                    GridItem(.flexible()),
-                    GridItem(.flexible()),
-                    GridItem(.flexible())
-                ], spacing: 10) {
+                    GridItem(.adaptive(minimum: 80))
+                ]) {
                     ForEach(colors, id: \.cgColor) { color in
                         Text("\(color)")
-                            .font(.system(size: 11, weight: .regular, design: .monospaced))
+                            .font(.system(size: 14, weight: .regular, design: .monospaced))
                             .foregroundColor(color)
                             .onReceive(inspection.notice) { self.inspection.visit(self, $0) }
                     }
@@ -40,11 +36,7 @@ struct BindingView: View {
             }
             VStack {
                 LazyVGrid(columns: [
-                    GridItem(.flexible()),
-                    GridItem(.flexible()),
-                    GridItem(.flexible()),
-                    GridItem(.flexible()),
-                    GridItem(.flexible())
+                    GridItem(.adaptive(minimum: 60))
                 ]) {
                     ForEach(0..<BindingView.numIcons, id: \.self) { index in
                         FigureRunView(colorGenerator: colorGenerator, color: $colors[index])
