e5f87436ce12771e9c96cf85ee89c5a75da96a15
Robert Gravina <robert.gravina@gmail.com>
2024-05-08 13:58:02 +1000

    Add button to simple state screen

--- SwiftUIPractice/Screens/SimpleStateView.swift
+++ SwiftUIPractice/Screens/SimpleStateView.swift
@@ -12,8 +12,14 @@ struct SimpleStateView: View {
 
     var body: some View {
         VStack {
-            Text("This view uses @State to store the color of the icon. Tap the icon to chose a random color.")
+            Text("""
+This view uses @State to store the color of the icon. Tap the icon to chose a random color.
+
+The button has no @State for color and a random color is chosen each time it is rendered.
+""")
             Spacer()
+            Button("Button") {
+            }.buttonStyle(PrimaryButtonStyle())
             Image(systemName: "figure.walk")
                 .resizable()
                 .scaledToFit()
