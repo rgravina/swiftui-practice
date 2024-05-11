fd0aa60c3dd1c1f4c7d77db19ea8170aa57b487b
Robert Gravina <robert.gravina@gmail.com>
2024-05-11 11:23:03 +1000

    Add start of coffee example

--- SwiftUIPractice/Screens/DIPBeforeView.swift
+++ SwiftUIPractice/Screens/DIPBeforeView.swift
@@ -1,21 +1,52 @@
 import SwiftUI
 
+struct Coffee: Identifiable {
+    let id = UUID()
+    var name: String
+}
+
 class DIPBeforeViewModel: ObservableObject {
+    @Published var coffee = Array<Coffee>()
+
+    init() {
+        self.coffee = [Coffee(name: "Espresso"), Coffee(name: "Espresso"), Coffee(name: "Espresso"), Coffee(name: "Espresso")]
+    }
+
+    func brewCoffee() {
+    }
 }
 
 struct DIPBeforeView: View {
+    let coffeeGridLayout = [GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible())]
     @StateObject var viewModel = DIPBeforeViewModel()
 
     var body: some View {
         VStack {
-            VStack {
-                ScrollView {
-                    Text("""
-TODO: This view contains an example not using DIP.
-""")
+            Button("Brew Coffee") {
+                viewModel.brewCoffee()
+            }.buttonStyle(PrimaryButtonStyle())
+            LazyVGrid(columns: coffeeGridLayout) {
+                ForEach(viewModel.coffee) { coffee in
+                    CoffeeView(coffeeName: coffee.name, imageName: "cup.and.saucer")
                 }
             }
+            Spacer()
         }
         .padding()
+    }
+}
+
+
+struct CoffeeView: View {
+    let coffeeName: String
+    let imageName: String
+    var body: some View {
+        VStack {
+            Text(coffeeName)
+            Image(systemName: imageName)
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.brown)
+        }
     }
 }
